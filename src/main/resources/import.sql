505e2bbc8ab179e77c01ff7081c0157cd0b5f2b9
João Paulo <joaopaulo4911@gmail.com>
2023-06-29 08:50:35 -0300

    Adicionado instrucoes sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,6 +13,10 @@ INSERT INTO transacao (valor, data, parcela, data_pagamento, data_vencimento, fa
 INSERT INTO transacao (valor, data, parcela, data_pagamento, data_vencimento, fatura_id) VALUES (50.00, CURRENT_TIMESTAMP, 1, DATE_ADD(CURDATE(), INTERVAL 30 DAY), DATE_ADD(CURDATE(), INTERVAL 60 DAY), 3);
 INSERT INTO transacao (valor, data, parcela, data_pagamento, data_vencimento, fatura_id) VALUES (100.00, CURRENT_TIMESTAMP, 1, DATE_ADD(CURDATE(), INTERVAL 30 DAY), DATE_ADD(CURDATE(), INTERVAL 60 DAY), 4);
 
+INSERT INTO transacao (valor, data, parcela, data_pagamento, data_vencimento, fatura_id) VALUES (999.00, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 20 DAY), 1, DATE_ADD(CURDATE(), INTERVAL 30 DAY), DATE_ADD(CURDATE(), INTERVAL 60 DAY), 1);
+INSERT INTO transacao (valor, data, parcela, data_pagamento, data_vencimento, fatura_id) VALUES (500.00, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 33 DAY), 1, DATE_ADD(CURDATE(), INTERVAL 30 DAY), DATE_ADD(CURDATE(), INTERVAL 60 DAY), 2);
+INSERT INTO transacao (valor, data, parcela, data_pagamento, data_vencimento, fatura_id) VALUES (200.00, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 100 DAY), 1, DATE_ADD(CURDATE(), INTERVAL 30 DAY), DATE_ADD(CURDATE(), INTERVAL 60 DAY), 3);
+
 INSERT INTO meta_categoria (categoria_id, limite, controle) VALUES (1, 10.0, 2);
 INSERT INTO meta_categoria (categoria_id, limite, controle) VALUES (2, 15.0, 1);
 INSERT INTO meta_categoria (categoria_id, limite, controle) VALUES (1, 10.0, 2);
