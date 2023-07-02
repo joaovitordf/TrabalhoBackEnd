0817280442bd8ca5a377a17def553c63c45d9ccf
joaovitordf <xjoaovitordf@gmail.com>
2023-07-02 09:51:20 -0300

    Rest API via Postman criado

--- src/main/java/com/example/sistemafinanceiro/api/repository/MetaCategoriaRepository.java
+++ src/main/java/com/example/sistemafinanceiro/api/repository/MetaCategoriaRepository.java
@@ -2,12 +2,9 @@ package com.example.sistemafinanceiro.api.repository;
 
 import com.example.sistemafinanceiro.api.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.sistemafinanceiro.api.model.MetaCategoria;
-
-import java.util.List;
 
 @Repository
 public interface MetaCategoriaRepository extends JpaRepository<MetaCategoria, Long>{
